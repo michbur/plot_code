ce020cb4bfe20644bfad701c773d6aa747779eb4
michbur <michalburdukiewicz@gmail.com>
2015-01-28 16:00:50 +0100

    story done

--- plot_code.R
+++ plot_code.R
@@ -17,15 +17,30 @@ names(gcode) <- c("TTT", "TTC", "TTA", "TTG", "TCT", "TCC",
                   "GGT", "GGC", "GGA", "GGG")
 
 nuc <- c("A", "C", "G", "T")
-nucdf <- data.frame(x = rep(1L:64, 3),
-                    y = c(rep(1, 64), rep(2, 64), rep(3, 64)),
-                    nuc = c(as.vector(sapply(nuc, rep, 16)),
-                            rep(as.vector(sapply(nuc, rep, 4)), 4),
-                            rep(nuc, 16)))
-ggplot(nucdf, aes(x = as.factor(x), y = y, fill = nuc)) + 
+
+snuc = c(as.vector(sapply(nuc, rep, 16)),
+         rep(as.vector(sapply(nuc, rep, 4)), 4),
+         rep(nuc, 16))
+
+codons <- apply(matrix(snuc, ncol = 3), 1, function(i) paste(i, collapse = ""))    
+
+
+
+nucdf <- data.frame(x = as.factor(rep(1L:64, 3)),
+                    y = c(rep(1, 64), rep(2, 64), rep(3, 64)))
+
+ggplot(nucdf, aes(x = x, y = y, fill = snuc)) + 
   geom_tile() +
   geom_abline(intercept = 1.5, slope = 0) +
   geom_abline(intercept = 2.5, slope = 0) +
   geom_abline(intercept = 3.5, slope = 0) +
+  theme_bw() +
+  scale_x_discrete(labels=unname(gcode[codons])) + 
+  scale_fill_hue("Nucleotides", c=40, l=80) + 
   coord_polar() +
-  element_blank()
+  theme(axis.ticks.y = element_blank(), axis.text.y = element_blank(),
+        axis.title.y = element_blank(), axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 15),
+        panel.border = element_blank(),
+        panel.grid.major = element_line(color = "gray60", size = 0.3),
+        panel.grid.major.y = element_blank())
